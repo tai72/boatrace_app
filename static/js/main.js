e09ac3d757517d94d0dfda7e237c3bb1ae2b00e5
tai72 <yamashi7227.04020105.7227@outlook.jp>
2022-08-17 19:21:23 +0900

    トップページまで

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+// ウィンドウサイズをウィンドウの高さにする
+
+$(document).ready(function() {
+    hsize = $(window).height();
+    $('body').css('height', hsize + 'px');
+});
+$(window).resize(function() {
+    hsize = $(window).height();
+    $('body').css('height', hsize + 'px');
+});
+
+console.log('a');
