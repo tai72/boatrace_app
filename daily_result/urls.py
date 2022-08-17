71365eb68148243b1698d3bc3c633a8516beb853
tai72 <yamashi7227.04020105.7227@outlook.jp>
2022-08-17 21:41:08 +0900

    計算結果を出力する雛形作成

--- daily_result/urls.py
+++ daily_result/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = 'daily_result'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('daily-result-form/', views.DailyResultFormView.as_view(), name="daily_result_form"),
+    path('daily-result', views.DailyResultView.as_view(), name="daily_result"),
 ]
--- daily_result/views.py
+++ daily_result/views.py
@@ -1,5 +1,27 @@
+from django.shortcuts import render
 from django.views import generic
 
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class DailyResultFormView(generic.TemplateView):
+    """GCPから一日の収支結果を取得し表示するためのフォーム."""
+    template_name = "form.html"
+
+    def post(self, request):
+        message = 'メッセージ受け取りしました！'
+        context = {
+            'race_date': request.POST['race_date'],
+            'place_id': request.POST['place_id'],
+            'race_no': request.POST['race_no'],
+        }
+
+        params = {
+            "benefit": "もうちょいまってねん",
+        }
+        return render(request, 'result.html', params)
+
+class DailyResultView(generic.TemplateView):
+    """フォーム情報を受け取り、表示する"""
+    template_name = "result.html"
