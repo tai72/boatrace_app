07c2857afb2c381243bdc723ac9c1f4d9b235c6d
tai72 <yamashi7227.04020105.7227@outlook.jp>
2022-10-29 20:58:46 +0900

    お問合せの設定

--- boatrace/settings.py
+++ boatrace/settings.py
@@ -186,5 +186,16 @@ LOGGING = {
 
 # 静的ファイルのパス設定
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'), 
 )
+
+# メールの配信先をコンソールにする設定
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# メールサーバー設定
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vision7227tata@gmail.com'
+EMAIL_HOST_PASSWORD = 'aexvpanoslwqaooi'
+EMAIL_USE_TLS = True
